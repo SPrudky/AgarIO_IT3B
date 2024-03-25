558a17f8d77c6e80567171d755ee5e79bc0b7a4e
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2024-03-25 10:36:04 +0100

    trochu se hýbe (musí se chytit)

--- AgarIO_IT3B/MainWindow.xaml.cs
+++ AgarIO_IT3B/MainWindow.xaml.cs
@@ -1,35 +1,61 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AgarIO_IT3B
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
-    }
-  
-    public void See()
+        private Player player;
+        private bool isMousePressed = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            canvasGame.MouseLeftButtonDown += CanvasGame_MouseLeftButtonDown;
+            canvasGame.MouseLeftButtonUp += CanvasGame_MouseLeftButtonUp;
+            canvasGame.MouseMove += CanvasGame_MouseMove;
+        }
+
+        private void CanvasGame_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isMousePressed = true;
+        }
+
+        private void CanvasGame_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isMousePressed = false;
+        }
+
+        private void CanvasGame_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMousePressed && player != null)
+            {
+                Point mousePosition = e.GetPosition(canvasGame);
+                player.Location = mousePosition;
+                UpdatePlayerUI();
+            }
+        }
+
+        private void See()
         {
             double y = canvasGame.ActualHeight / 2;
             double x = canvasGame.ActualWidth / 2;
 
-            Player player = new Player(Brushes.Blue, "Martin")
+            player = new Player(Brushes.Blue, "Martin")
             {
-                Location = new Point(x,y),
+                Location = new Point(x, y),
             };
+            UpdatePlayerUI();
+        }
+
+        private void UpdatePlayerUI()
+        {
+            canvasGame.Children.Clear();
+
             Ellipse ellipse = new Ellipse()
             {
                 Fill = player.Color,
@@ -37,8 +63,13 @@ namespace AgarIO_IT3B
                 Height = player.Size,
             };
             canvasGame.Children.Add(ellipse);
-            Canvas.SetLeft(ellipse, player.Location.X);
-            Canvas.SetTop(ellipse, player.Location.Y);
+            Canvas.SetLeft(ellipse, player.Location.X - player.Size / 2);
+            Canvas.SetTop(ellipse, player.Location.Y - player.Size / 2);
         }
-  }
+
+        private void canvasGame_Loaded(object sender, RoutedEventArgs e)
+        {
+            See();
+        }
+    }
 }
