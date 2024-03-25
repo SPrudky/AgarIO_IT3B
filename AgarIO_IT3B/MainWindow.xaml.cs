6caa174ada1ce08bfbab48f5a61aa49c5a461d34
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2024-03-25 10:21:41 +0100

    Přidejte soubory projektu.

---
+++ AgarIO_IT3B/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgarIO_IT3B
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  
+    public void See()
+        {
+            double y = canvasGame.ActualHeight / 2;
+            double x = canvasGame.ActualWidth / 2;
+
+            Player player = new Player(Brushes.Blue, "Martin")
+            {
+                Location = new Point(x,y),
+            };
+            Ellipse ellipse = new Ellipse()
+            {
+                Fill = player.Color,
+                Width = player.Size,
+                Height = player.Size,
+            };
+            canvasGame.Children.Add(ellipse);
+            Canvas.SetLeft(ellipse, player.Location.X);
+            Canvas.SetTop(ellipse, player.Location.Y);
+        }
+  }
+}
---
+++ AgarIO_IT3B/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace AgarIO_IT3B
+{
+  public class Player
+  {
+    public double Size { get; set; }
+    //public double Speed { get; set; }
+    public Brush Color { get; set; }
+    public string Name { get; set; }
+    public Point Location { get; set; }
+
+    public Player(Brush color, string name)
+    {
+      Color = color;
+      Name = name;
+      Size = 10;
+      Location = new Point(0, 0);
+    }
+  }
+}
